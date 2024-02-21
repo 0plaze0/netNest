951b8fbc04eaa51974d9faef8a89f2ad82d62096
plaze <banikakash131@gmail.com>
2024-02-21 17:36:02 +0530

    feat: add middleware

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
+import morgan from "morgan";
 
 import connectDB from "./config/dbConn.js";
 
@@ -8,6 +9,11 @@ connectDB();
 
 const PORT = process.env.PORT || 8080;
 const app = express();
+
+//middleware
+
+app.use(express.json());
+app.use(morgan("dev"));
 
 //routes
 app.get("/", (req, res) => {
