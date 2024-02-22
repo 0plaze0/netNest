94e76a3fabcb65901a1322e7c429084f701cdd7e
plaze <banikakash131@gmail.com>
2024-02-22 19:45:55 +0530

    feat: add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
+import { Layout } from "./components/index";
+import { Home, About, Contact, Policy, Pagenotfound } from "./pages/index";
 import "./App.scss";
 
 function App() {
-  return <div className="app">Hello world</div>;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/policy" element={<Policy />} />
+          <Route path="/*" element={<Pagenotfound />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { Header, Footer } from "./../index";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <div className="app__layout">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
